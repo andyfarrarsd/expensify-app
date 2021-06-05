2c0cfa3365685d04b72c65b3366c791e2c1b1611
Andy Farrar <andy.farrar.sd@gmail.com>
2021-06-05 13:17:07 -0700

    Set up devDependancies and dist folder

--- start-babel.sh
+++ start-babel.sh
@@ -3,3 +3,14 @@ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
 yarn run dev-server  // to restart the dev server
 
 yarn test -- --watch  // start the test script in watch mode in the background
+
+yarn install --production // for just the production dependanices
+yarn install // for all dev and prod dependanices
+
+yarn run build:prod // to build the app and all dependacies
+
+yarn run start // start the production server
+
+git commit -a -m " comment  "   // to check in modified files
+// need to run git add . // If there are new files
+
